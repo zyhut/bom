bfe6b1a28e94ba5b83916ca3ee8346bc00b0ff51
zyhut <code@zyhut.com>
2025-03-25 22:49:40 -0700

    Refine payment's UX

--- components/ThemedButton.tsx
+++ components/ThemedButton.tsx
@@ -6,17 +6,20 @@ import { ThemedText } from './ThemedText';
 type Props = {
   children: React.ReactNode;
   onPress: () => void;
+  textColor?: string;
+  theme?: any;
   style?: any;
 };
 
-export const ThemedButton = ({ children, onPress, style }: Props) => {
+export const ThemedButton = ({ children, onPress, textColor, theme, style }: Props) => {
   const { colors } = useTheme();
 
   return (
     <Button
       mode="outlined"
-      textColor={colors.primary}
+      textColor={textColor || colors.primary}
       style={[{ borderRadius: 8 }, style]}
+      theme={theme}
       onPress={onPress}
     >
       {children}
